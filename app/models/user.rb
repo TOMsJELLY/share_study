ba9be286e2a5d44d8636c508fb8c5d4052523f68
TOMsJELLY <rasty8ilknods@gmail.com>
2020-11-10 16:39:02 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :grade
-
   validates :nickname,      presence: true
   validates :email,         presence: true,     uniqueness: { case_sensitive: :false }
   validates :password,      presence: true,     format: {with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/}
